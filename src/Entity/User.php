880c701309758607731384bb0009ef02a1dabe6b
Carpe-frite <jordi.guelpa@protonmail.com>
2025-10-07 15:27:23 +0200

    entity

---
+++ src/Entity/User.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $user_id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $user_name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $user_email = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTime $user_dob = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(int $user_id): static
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getUserName(): ?string
+    {
+        return $this->user_name;
+    }
+
+    public function setUserName(string $user_name): static
+    {
+        $this->user_name = $user_name;
+
+        return $this;
+    }
+
+    public function getUserEmail(): ?string
+    {
+        return $this->user_email;
+    }
+
+    public function setUserEmail(string $user_email): static
+    {
+        $this->user_email = $user_email;
+
+        return $this;
+    }
+
+    public function getUserDob(): ?\DateTime
+    {
+        return $this->user_dob;
+    }
+
+    public function setUserDob(?\DateTime $user_dob): static
+    {
+        $this->user_dob = $user_dob;
+
+        return $this;
+    }
+}
